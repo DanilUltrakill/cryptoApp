2a144e16dc56d93c51e3a68c73c4897f26febd24
DanilUltrakill <yaremakadanil@gmail.com>
2024-05-20 00:00:28 +0700

    mapper, rename, 1 usecase, repository impl, changed to suspend func

---
+++ app/src/main/java/com/example/cryptoapp/data/mapper/CoinMapper.kt
@@ -0,0 +1,57 @@
+package com.example.cryptoapp.data.mapper
+
+import com.example.cryptoapp.data.database.CoinInfoDbModel
+import com.example.cryptoapp.data.network.model.CoinInfoDto
+import com.example.cryptoapp.data.network.model.CoinInfoJsonContainerDto
+import com.example.cryptoapp.data.network.model.CoinNamesListDto
+import com.example.cryptoapp.domain.entity.CoinInfo
+import com.google.gson.Gson
+
+class CoinMapper {
+
+    fun mapDtoToDbModel(dto: CoinInfoDto) = CoinInfoDbModel(
+        fromSymbol = dto.fromSymbol,
+        toSymbol = dto.toSymbol,
+        price = dto.price,
+        lastMarket = dto.lastMarket,
+        lastUpdate = dto.lastUpdate,
+        highDay = dto.highDay,
+        lowDay = dto.lowDay,
+        imageUrl = dto.imageUrl
+    )
+
+    fun mapJsonContainerToListCoinInfo(jsonContainer: CoinInfoJsonContainerDto): List<CoinInfoDto> {
+        val result = mutableListOf<CoinInfoDto>()
+        val jsonObject = jsonContainer.json ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinInfoDto::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    fun mapNamesListToString(namesListDto: CoinNamesListDto): String {
+        return namesListDto.names?.map {
+            it.coinName?.name
+        }?.joinToString(",") ?: ""
+    }
+
+    fun mapDbModelToEntity(dbModel: CoinInfoDbModel) = CoinInfo(
+        fromSymbol = dbModel.fromSymbol,
+        toSymbol = dbModel.toSymbol,
+        price = dbModel.price,
+        lastMarket = dbModel.lastMarket,
+        lastUpdate = dbModel.lastUpdate,
+        highDay = dbModel.highDay,
+        lowDay = dbModel.lowDay,
+        imageUrl = dbModel.imageUrl
+    )
+}
